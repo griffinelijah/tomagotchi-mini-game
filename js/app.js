785543f94e00b948bf1e3eb80f3160f5f4c18b50
Griffin Delgado <griffindelgado@Griffins-MacBook-Air.local>
2019-10-01 15:31:12 -0500

    instantiated the tomagotchi

---
+++ js/app.js
@@ -0,0 +1,31 @@
+class tomagotchi {
+	constructor(name){
+		this.name = name,
+		this.age = 2,
+		this.hunger = 10,
+		this.sleepiness = 10,
+		this.boredom = 10
+	}
+	//methods for feeding, dying, sleeping etc
+}
+
+const tom = new tomagotchi('tom')
+console.log(tom);
+const game = {
+	start(){
+
+	}
+	//game stuff
+	//method to start timer - setInterval
+
+
+
+	//printValues()
+		//new valuse of the tomagotchi
+
+}
+
+
+
+//inster listeners here. $().on('click')
+$('.start').on('click')
