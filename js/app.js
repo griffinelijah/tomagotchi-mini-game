4eddc3ec134270e1475c0f8f7765c488755d6bc0
Griffin Delgado <griffindelgado@Griffins-MacBook-Air.local>
2019-10-01 20:08:45 -0500

    updated timer to include values from other stats

--- js/app.js
+++ js/app.js
@@ -2,22 +2,42 @@ class tomagotchi {
 	constructor(name){
 		this.name = name,
 		this.age = 2,
-		this.hunger = 10,
-		this.sleepiness = 10,
-		this.boredom = 10
+		this.hunger = 1,
+		this.sleepiness = 1,
+		this.boredom = 1
 	}
 	//methods for feeding, dying, sleeping etc
 }
 
 const tom = new tomagotchi('tom')
 
-
 console.log(tom);
+
 const game = {
 	pet: tom,
+	time: 10,
 	start(){
-		const $tomPic = $('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2BvXA_TY0edchJonvgYNceml5sm9qjMgE-Tc6PHVFQAffH90Lxg"></img>').appendTo('body')
-		const $tomStats = $(`<ul class="tom-stats"><li>${tom.name}</li><li>${tom.age}</li><li>${tom.hunger}</li><li>${tom.sleepiness}</li><li>${tom.boredom}</li></ul>`).appendTo('body')
+		const $tomPic = $('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQquAoMnxzaw9H0H_U95d6DnxRdEdqWjzGW94gsUeK9Z3ljHAD9"></img>').appendTo('body')
+		// const $tomStats = $(`<ul class="tom-stats"><li>${tom.name}</li><li>${tom.age}</li><li>${tom.hunger}</li><li>${tom.sleepiness}</li><li>${tom.boredom}</li></ul>`).appendTo('body')
+	},//timer to increment age value as ttime goes on
+	//will also be used later on to determine hunger, boredom and sleepiness levels
+	setTimer(){
+		const $age = $('#tomAge');
+		const $hunger = $('#tomHunger');
+		const $sleep = $('#tomSleepiness');
+		const $bored = $('#tomBoredom');
+		const interval = setInterval(() => {
+			tom.age++
+			tom.hunger++
+			tom.sleepiness++
+			tom.boredom++
+
+		$age.text(`age: ${tom.age}`)
+		$hunger.text(`hunger: ${tom.hunger}`)
+		$sleep.text(`sleepiness: ${tom.sleepiness}`)
+		$bored.text(`boredom: ${tom.boredom}`)
+			
+		}, 1000)
 	}
 	//game stuff
 	//method to start timer - setInterval
@@ -32,9 +52,9 @@ const game = {
 
 
 //inster listeners here. $().on('click')
-$('button').on('click', () => {
+$('.start').on('click', () => {
 	game.start();
-
+	game.setTimer();
 
 	
 })
